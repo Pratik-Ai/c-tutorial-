d9bd01c51c2b9364d1bf8302410c9742eb381141
Pratik Pawar <76660388+Pratik-Ai@users.noreply.github.com>
2021-01-13 23:22:33 +0530

    Create if else.c

---
+++ if else.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+//applying if statement
+{
+printf("enter the percentage");
+int i;
+scanf("%d",&i);
+if(i<35)
+//student fail
+printf("fail");
+else
+printf('pass');
+}
